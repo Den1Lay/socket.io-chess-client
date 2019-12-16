b833290fd21cde9fc64feb2f4024506d823ee0aa
Des_Flex <denilay13902@gmail.com>
2019-12-16 12:38:51 +0500

    Updates

---
+++ precache-manifest.49e6a3d40bc574062a138c0ccb4ba416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2816df9cfa2e6307d299ab2fc1e69754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22c40c613e621ec5f218",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "5571bbe2af1af4d1781b",
+    "url": "/static/css/main.021a3c1b.chunk.css"
+  },
+  {
+    "revision": "22c40c613e621ec5f218",
+    "url": "/static/js/2.838256e3.chunk.js"
+  },
+  {
+    "revision": "5571bbe2af1af4d1781b",
+    "url": "/static/js/main.2010afa0.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "7a4628fefe313b9efdfb0a8945b0f1c2",
+    "url": "/static/media/picture.7a4628fe.png"
+  }
+]);
